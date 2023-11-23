7489db7c6d35f75a5c351204583a52f57470af09
PakornBank <pakorn22120@gmail.com>
2023-11-23 13:40:44 +0700

    Add headings and styling to index.css and main.jsx

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -9,9 +9,29 @@ import Buttons from './Buttons.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1>React Demo</h1>
+
+    <h2>My first jsx! &#x2193; &#x2193;
+    &#x2193;
+    </h2>
     <Hello />
+
+    <h2>Creating a list of my favorite foods &#x2193;
+&#x2193;
+&#x2193;
+</h2>
     <Fav />
-    <Convert></Convert>
+
+    <h2>Convert HTML to jsx &#x2193;
+&#x2193;
+&#x2193;
+</h2>
+    <Convert />
+
+    <h2>React state &#x2193;
+&#x2193;
+&#x2193;
+</h2>
     <Buttons></Buttons>
   </React.StrictMode>,
 )
